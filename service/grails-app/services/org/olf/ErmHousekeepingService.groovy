e1e545a9569779447dfa0fdc5a304f4f1383e0e0
Ian Ibbotson <ianibbo@gmail.com>
2018-03-28 15:39:22 +0100

    Pre-merge commit

--- service/grails-app/services/org/olf/ErmHousekeepingService.groovy
+++ service/grails-app/services/org/olf/ErmHousekeepingService.groovy
@@ -11,29 +11,35 @@ import grails.gorm.multitenancy.WithoutTenant
 /**
  * This service works at the module level, it's often called without a tenant context.
  */
+@WithoutTenant
 class ErmHousekeepingService {
 
+  /**
+   * This is called by the eventing mechanism - There is no web request context
+   * there may be something funky to do with events and multi tenant setup.
+   */
   @Subscriber('okapi:schema_update')
-  void onSchemaUpdate(tenantId, schemaName) {
+  void onSchemaUpdate(called_tenantId, schemaName) {
     log.debug("ErmHousekeepingService::onSchemaUpdate(${tenantId},${schemaName})");
+    setupData(called_tenantId, schemaName);
   }
 
-  private void setupData(enantId, schemaName) {
+  private void setupData(called_tenantId, schemaName) {
 
     // Skip this until we can work out whats going wrong...
-    Tenants.withId(schemaName) {
-          // A special record for packages which are really defined locally - this is an exceptional situation
-          RemoteKB local_kb = RemoteKB.findByName('LOCAL') ?: new RemoteKB( name:'LOCAL',
-                                                                            type:'LOCAL',
-                                                                            rectype: new Long(1),
-                                                                            active:Boolean.TRUE).save(flush:true, failOnError:true);
-    
-          // A default record for remote GOKb - Switched off by default, will need to add other properties shortly
-          RemoteKB gokb = RemoteKB.findByName('GOKb') ?: new RemoteKB( name:'GOKb',
-                                                                       type:'kb-oai',
-                                                                       rectype: new Long(1),
-                                                                       active:Boolean.FALSE).save(flush:true, failOnError:true);
-    }
+    Tenant.withId(schemaName) {
 
+      // A special record for packages which are really defined locally - this is an exceptional situation
+      RemoteKB local_kb = RemoteKB.findByName('LOCAL') ?: new RemoteKB( name:'LOCAL',
+                                                                        type:'LOCAL',
+                                                                        rectype: new Long(1),
+                                                                        active:Boolean.TRUE).save(flush:true, failOnError:true);
+    
+      // A default record for remote GOKb - Switched off by default, will need to add other properties shortly
+      RemoteKB gokb = RemoteKB.findByName('GOKb') ?: new RemoteKB( name:'GOKb',
+                                                                   type:'kb-oai',
+                                                                   rectype: new Long(1),
+                                                                   active:Boolean.FALSE).save(flush:true, failOnError:true);
+    }
   }
 }
