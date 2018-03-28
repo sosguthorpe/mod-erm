3ae07bd46dbb240650f8e61bf7623174e1aa0d62
Ian Ibbotson <ianibbo@gmail.com>
2018-03-28 16:13:51 +0100

    Use okaip-grails 3.3.6 - tests return RemoteKB instances

--- service/grails-app/services/org/olf/ErmHousekeepingService.groovy
+++ service/grails-app/services/org/olf/ErmHousekeepingService.groovy
@@ -1,28 +1,31 @@
 package org.olf
 
-import static grails.gorm.multitenancy.Tenants.*
-
+import grails.gorm.multitenancy.Tenants
 import org.olf.kb.RemoteKB
-
-import grails.events.annotation.*
+import grails.events.annotation.Subscriber
 import grails.gorm.multitenancy.WithoutTenant
-
+import grails.gorm.transactions.Transactional
 
 /**
  * This service works at the module level, it's often called without a tenant context.
  */
-@WithoutTenant
-class ErmHousekeepingService {
+@Transactional
+public class ErmHousekeepingService {
 
   /**
    * This is called by the eventing mechanism - There is no web request context
    * there may be something funky to do with events and multi tenant setup.
    */
   @Subscriber('okapi:schema_update')
-  void onSchemaUpdate(tenantName, tenantId) {
-    log.debug("ErmHousekeepingService::onSchemaUpdate(${tenantName},${tenantId})")
+  public void onSchemaUpdate(tn, tid) {
+    log.debug("ErmHousekeepingService::onSchemaUpdate(${tn},${tid})")
     // Skip this until we can work out whats going wrong...
-    withId(tenantId) {
+    setupData(tn, tid);
+  }
+
+  private void setupData(tenantName, tenantId) {
+
+    Tenants.withId(tenantId) {
       // A special record for packages which are really defined locally - this is an exceptional situation
       RemoteKB local_kb = RemoteKB.findByName('LOCAL') ?: new RemoteKB( name:'LOCAL',
                                                                         type:'LOCAL',
