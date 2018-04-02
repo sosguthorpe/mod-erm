12d8b22b6302f0fd638996c5a7e0ff25ee58b41e
Ian Ibbotson <ianibbo@gmail.com>
2018-04-02 20:10:52 +0100

    Working on kb domain model, added integration test and test data packages

---
+++ service/grails-app/domain/org/olf/kb/PackageContentItem.groovy
@@ -0,0 +1,28 @@
+package org.olf.kb
+
+import grails.gorm.MultiTenant
+
+
+/**
+ * mod-erm representation of a package
+ */
+public class PackageContentItem implements MultiTenant<Package> {
+
+  String id
+  Package pkg
+  PlatformTitleInstance pti
+
+  static mapping = {
+                   id column:'pci_id', generator: 'uuid', length:36
+              version column:'pci_version'
+                  pkg column:'pci_pkg_fk'
+                  pti column:'pci_pti_fk'
+  }
+
+  static constraints = {
+    pkg(nullable:false, blank:false)
+    pti(nullable:false, blank:false)
+  }
+
+
+}
