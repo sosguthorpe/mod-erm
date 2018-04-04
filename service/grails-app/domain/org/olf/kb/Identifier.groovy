a582565860630f213ae233617aa93bdff11c4bc1
Ian Ibbotson <ianibbo@gmail.com>
2018-04-04 10:45:04 +0100

    Added identifier

---
+++ service/grails-app/domain/org/olf/kb/Identifier.groovy
@@ -0,0 +1,28 @@
+package org.olf.kb
+
+import grails.gorm.MultiTenant
+
+
+/**
+ *
+ */
+public class Identifier implements MultiTenant<Identifier> {
+
+  String id
+  String value
+  IdentifierNamespace ns
+
+  static mapping = {
+                   id column:'id_id', generator: 'uuid', length:36
+              version column:'id_version'
+                 name column:'id_value'
+                   ns column:'id_ns_fk'
+  }
+
+  static constraints = {
+          value(nullable:false, blank:false)
+             ns(nullable:false, blank:false)
+  }
+
+
+}
