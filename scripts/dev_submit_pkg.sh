8382813a2624a7d49e1ca1edb782f0648a0d9836
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-04-08 10:35:16 +0100

    updated

--- scripts/dev_submit_pkg.sh
+++ scripts/dev_submit_pkg.sh
@@ -6,3 +6,5 @@
 # Create DevTeanantA
 curl --header "X-Okapi-Tenant: DevTenantA" http://localhost:8080/_/tenant -X POST
 curl --header "X-Okapi-Tenant: DevTenantA" -X POST -F package_file=@../service/src/integration-test/resources/packages/apa_1062.json http://localhost:8080/admin/loadPackage
+
+
