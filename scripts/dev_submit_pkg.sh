d0982dd41c49b3efa02d9bcd1dee8db11c24a714
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-04-08 16:40:41 +0100

    Added working example of using curl to create a new agreement to scripts/dev_submit_pkg.sh

--- scripts/dev_submit_pkg.sh
+++ scripts/dev_submit_pkg.sh
@@ -7,4 +7,8 @@
 curl --header "X-Okapi-Tenant: DevTenantA" http://localhost:8080/_/tenant -X POST
 curl --header "X-Okapi-Tenant: DevTenantA" -X POST -F package_file=@../service/src/integration-test/resources/packages/apa_1062.json http://localhost:8080/admin/loadPackage
 
-
+curl --header "X-Okapi-Tenant: DevTenantA" -H "Content-Type: application/json" -X POST http://localhost:8080/sas -d '
+{
+  name: "A new agreement"
+}
+'
