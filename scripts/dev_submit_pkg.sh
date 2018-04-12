e5c08b9a678daca07b8290f41160c12e43e56c42
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-04-12 23:05:18 +0100

    Return package IDs when loading and grab them in the scripts

--- scripts/dev_submit_pkg.sh
+++ scripts/dev_submit_pkg.sh
@@ -7,7 +7,9 @@
 curl --header "X-Okapi-Tenant: diku" http://localhost:8080/_/tenant -X POST
 
 # Prepolpulate with data.
-curl --header "X-Okapi-Tenant: diku" -X POST -F package_file=@../service/src/integration-test/resources/packages/apa_1062.json http://localhost:8080/admin/loadPackage
+BSEC_PKG_ID=`curl --header "X-Okapi-Tenant: diku" -X POST -F package_file=@../service/src/integration-test/resources/packages/bentham_science_bentham_science_eduserv_complete_collection_2015_2017_1386.json http://localhost:8080/admin/loadPackage | jq ".newPackageId"`
+
+APA_PKG_ID=`curl --header "X-Okapi-Tenant: diku" -X POST -F package_file=@../service/src/integration-test/resources/packages/apa_1062.json http://localhost:8080/admin/loadPackage | jq ".newPackageId"`
 
 # Create an agreement
 curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" -X POST http://localhost:8080/sas -d '
@@ -20,11 +22,12 @@ curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" -X POST
 AGREEMENT_ID=`curl --header "X-Okapi-Tenant: diku" http://localhost:8080/sas -X GET | jq ".[0].id"`
 
 # List packages
-PACKAGE_ID=`curl --header "X-Okapi-Tenant: diku" http://localhost:8080/packages -X GET | jq ".[0].id"`
+# We now get the package back when we load the package above, this is still a cool way to work tho
+# PACKAGE_ID=`curl --header "X-Okapi-Tenant: diku" http://localhost:8080/packages -X GET | jq ".[0].id"`
 
 curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" -X POST http://localhost:8080/sas/$AGREEMENT_ID/addToAgreement -d ' {
   content:[
-    { "type":"package", "id": "'"$PACKAGE_ID"'" }
+    { "type":"package", "id": "'"$APA_PKG_ID"'" }
   ]
 }
 '
