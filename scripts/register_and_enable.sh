f288bde2568844c0483a3b5a3ff3d3db48278900
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-04-12 15:38:49 +0100

    App now names olf-erm.

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -1,9 +1,9 @@
 pushd ../service
-curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/service-1.0.0
-curl -XDELETE http://localhost:9130/_/discovery/modules/service-1.0.0/localhost-service-1.0.0
-curl -XDELETE http://localhost:9130/_/proxy/modules/service-1.0.0
+curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/olf-erm-1.0.0
+curl -XDELETE http://localhost:9130/_/discovery/modules/olf-erm-1.0.0/localhost-olf-erm-1.0.0
+curl -XDELETE http://localhost:9130/_/proxy/modules/olf-erm-1.0.0
 ./gradlew generateDescriptors
 curl -XPOST http://localhost:9130/_/proxy/modules -d @../service/build/resources/okapi/ModuleDescriptor.json
 curl -XPOST http://localhost:9130/_/discovery/modules -d @../service/build/resources/okapi/DeploymentDescriptor.json
-curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "service-1.0.0"}'
+curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "olf-erm-1.0.0"}'
 popd
