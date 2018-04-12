9387f346d46e71cc67497fa99516e08179e28553
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-04-12 16:02:13 +0100

    Added a clean.

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -2,7 +2,7 @@ pushd ../service
 curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/olf-erm-1.0.0
 curl -XDELETE http://localhost:9130/_/discovery/modules/olf-erm-1.0.0/localhost-olf-erm-1.0.0
 curl -XDELETE http://localhost:9130/_/proxy/modules/olf-erm-1.0.0
-./gradlew generateDescriptors
+./gradlew clean generateDescriptors
 curl -XPOST http://localhost:9130/_/proxy/modules -d @../service/build/resources/okapi/ModuleDescriptor.json
 curl -XPOST http://localhost:9130/_/discovery/modules -d @../service/build/resources/okapi/DeploymentDescriptor.json
 curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "olf-erm-1.0.0"}'
