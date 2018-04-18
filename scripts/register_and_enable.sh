41f9a6167c7e5d0b33b51c3cda41d8bb441ef543
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-04-18 16:37:58 +0100

    Ensure correct path for script.

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -1,4 +1,5 @@
-pushd ../service
+BASEDIR=$(dirname "$0")
+pushd "$BASEDIR/../service"
 curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/olf-erm-1.0.0
 curl -XDELETE http://localhost:9130/_/discovery/modules/olf-erm-1.0.0/localhost-olf-erm-1.0.0
 curl -XDELETE http://localhost:9130/_/proxy/modules/olf-erm-1.0.0
