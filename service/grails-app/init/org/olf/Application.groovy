7354cf9d46be5b37aea2fea713c22f1fe9554897
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-06-08 09:56:10 +0100

    Add new builder customizer.

--- service/grails-app/init/org/olf/Application.groovy
+++ service/grails-app/init/org/olf/Application.groovy
@@ -2,9 +2,27 @@ package org.olf
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import io.undertow.Undertow
+import io.undertow.UndertowOptions
+import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory
+import org.springframework.context.annotation.Bean
 
 class Application extends GrailsAutoConfiguration {
   static void main(String[] args) {
     GrailsApp.run(Application, args)
+  }
+  
+  @Bean
+  UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory(){
+      UndertowEmbeddedServletContainerFactory factory= new UndertowEmbeddedServletContainerFactory();
+      factory.addBuilderCustomizers(new UndertowBuilderCustomizer(){
+          @Override public void customize(Undertow.Builder builder){
+              builder
+                .setWorkerThreads(64)
+              // There are many more options, see the documentation for more.
+          }
+      });
+      return factory;
   }
 }
