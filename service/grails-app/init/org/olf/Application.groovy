d7bc7c3715f715175e2fa77be2da12ffeaa1df7d
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-06-08 11:21:08 +0100

    Try without bean def.

--- service/grails-app/init/org/olf/Application.groovy
+++ service/grails-app/init/org/olf/Application.groovy
@@ -12,17 +12,4 @@ class Application extends GrailsAutoConfiguration {
   static void main(String[] args) {
     GrailsApp.run(Application, args)
   }
-  
-  @Bean
-  UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory(){
-      UndertowEmbeddedServletContainerFactory factory= new UndertowEmbeddedServletContainerFactory();
-      factory.addBuilderCustomizers(new UndertowBuilderCustomizer(){
-          @Override public void customize(Undertow.Builder builder){
-              builder
-                .setWorkerThreads(64)
-              // There are many more options, see the documentation for more.
-          }
-      });
-      return factory;
-  }
 }
