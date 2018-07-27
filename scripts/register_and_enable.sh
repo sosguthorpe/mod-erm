7498fd1aac0702884e2d0488b40cc031624ed813
Ian Ibbotson <ianibbo@gmail.com>
2018-07-27 10:37:00 +0100

    updated register_and_enable.sh

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -4,7 +4,7 @@ curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/olf-erm-1.0.0
 curl -XDELETE http://localhost:9130/_/discovery/modules/olf-erm-1.0.0/localhost-olf-erm-1.0.0
 curl -XDELETE http://localhost:9130/_/proxy/modules/olf-erm-1.0.0
 ./gradlew clean generateDescriptors
-curl -XPOST http://localhost:9130/_/proxy/modules -d @../service/build/resources/okapi/ModuleDescriptor.json
-curl -XPOST http://localhost:9130/_/discovery/modules -d @../service/build/resources/okapi/DeploymentDescriptor.json
+curl -XPOST http://localhost:9130/_/proxy/modules -d @../service/build/resources/main/okapi/ModuleDescriptor.json
+curl -XPOST http://localhost:9130/_/discovery/modules -d @../service/build/resources/main/okapi/DeploymentDescriptor.json
 curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "olf-erm-1.0.0"}'
 popd
