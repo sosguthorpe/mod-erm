d9081b572837ece7831cc4e7807aed45797a4760
Ian Ibbotson <ianibbo@gmail.com>
2018-08-03 08:01:13 +0100

    Refine what is presented for package content item

--- scripts/test_via_okapi.sh
+++ scripts/test_via_okapi.sh
@@ -9,3 +9,6 @@ echo Call olf-erm through okapi
 
 curl --header "X-Okapi-Tenant: diku" http://localhost:9130/content -X GET
 
+curl --header "X-Okapi-Tenant: diku" http://localhost:9130/ermpci?page=1&perPage=10&stats=true -X GET
+curl --header "X-Okapi-Tenant: diku" http://localhost:8080/ermpci?page=1&perPage=10&stats=true -X GET
+
