d3b4a92f5c1877f51a629971904cbeb2266132e9
Ian Ibbotson <ianibbo@gmail.com>
2018-08-05 16:56:09 +0100

    Working on scheduling KB sync tasks

---
+++ service/grails-app/services/org/olf/KBHarvestService.groovy
@@ -0,0 +1,37 @@
+package org.olf
+
+import grails.gorm.transactions.Transactional
+import org.springframework.scheduling.annotation.Scheduled
+import java.text.SimpleDateFormat
+import groovy.transform.CompileStatic
+import groovy.transform.CompileDynamic
+import groovy.util.logging.Slf4j
+
+
+/**
+ * See http://guides.grails.org/grails-scheduled/guide/index.html for info on this way of
+ * scheduling tasks
+ */
+@Slf4j 
+@CompileStatic 
+@Transactional
+class KBHarvestService {
+
+  @Scheduled(fixedDelay = 45000L, initialDelay = 5000L) 
+  void triggerSync() {
+    log.info "Simple Job every 45 seconds :{}", new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date())
+  }
+
+  // @CompileDynamic
+  // Date startAtDate() { 
+  //   Date startAt = new Date()
+  //   use(TimeCategory) {
+  //     startAt = startAt + 1.minute
+  //   }
+  //   startAt
+  // }
+
+  // void scheduleFollowupEmail(String email, String message) {
+    // threadPoolTaskScheduler.schedule(new EmailTask(emailService, email, message), startAtDate()) 
+  // }
+}
