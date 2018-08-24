e2e872bda97698ec27214d26e2b151ab13b1b8e8
Ian Ibbotson <ianibbo@gmail.com>
2018-08-24 11:50:25 +0100

    TitleInstance getEntitlements now returns the list of AgreementLineItems that relate to that title - so we can show users how they get access

--- service/grails-app/domain/org/olf/kb/TitleInstance.groovy
+++ service/grails-app/domain/org/olf/kb/TitleInstance.groovy
@@ -1,13 +1,25 @@
 package org.olf.kb
 
 import grails.gorm.MultiTenant
-
+import javax.persistence.Transient
+import org.olf.erm.AgreementLineItem
 
 /**
  * mod-erm representation of a BIBFRAME instance
  */
 public class TitleInstance implements MultiTenant<TitleInstance> {
 
+  private static final String ENTITLEMENTS_QUERY = '''from AgreementLineItem as ali 
+where exists ( select pci.id 
+               from PackageContentItem as pci
+               where pci.pti.titleInstance = :title 
+               and ali.pkg = pci.pkg )
+   or exists ( select pci.id 
+               from PackageContentItem as pci
+               where pci.pti.titleInstance = :title
+               and ali.pci = pci )
+   or exists ( select single_title from AgreementLineItem as single_title where single_title=ali and single_title.pti.titleInstance = :title )
+'''
 
   String id
   // Title IN ORIGINAL LANGUAGE OF PUBLICATION
@@ -32,4 +44,12 @@ public class TitleInstance implements MultiTenant<TitleInstance> {
   ]
 
 
+  /**
+   * Return the list of entitlements that grant us access to this title.
+   */
+  @Transient
+  List<AgreementLineItem> getEntitlements() {
+    List<AgreementLineItem> result = AgreementLineItem.executeQuery('select ali.id '+ENTITLEMENTS_QUERY,[title:this],[max:20, offset:0]);
+    return result;
+  }
 }
