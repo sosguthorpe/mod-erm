9d591dd4c83dedb02ec6b70f548b10b43ed5ce61
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-09-24 14:16:57 +0100

    Remove legacy props from refdata. We can expand as needed.

--- service/grails-app/domain/org/olf/general/RefdataCategory.groovy
+++ service/grails-app/domain/org/olf/general/RefdataCategory.groovy
@@ -17,18 +17,18 @@ class RefdataCategory implements MultiTenant<RefdataCategory> {
   static constraints = {
   }
 
-  static def lookupOrCreate(category_name, value, label=null, icon=null, sortKey=null) {
-    def cat = RefdataCategory.findByDesc(category_name);
-    if ( !cat ) {
-      cat = new RefdataCategory(desc:category_name).save(flush:true, failOnError:true);
-    }
+  static <T extends RefdataValue> T lookupOrCreateValue(final String category_name, final String value, final String label=null, Class<T> clazz = RefdataValue) {
+    RefdataCategory cat = RefdataCategory.findOrCreateByDesc(category_name).save(flush:true, failOnError:true)
+//    if ( !cat ) {
+//      cat = new RefdataCategory(desc:category_name).save(flush:true, failOnError:true)
+//    }
 
-    def result = RefdataValue.findByOwnerAndValue(cat, value)
+    T result = clazz.findOrCreateByOwnerAndValue(cat, value).save(flush:true, failOnError:true)
 
-    if ( !result ) {
-      new RefdataValue(owner:cat, value:value, label:(label?:value),icon:icon, sortKey:sortKey).save(flush:true);
-      result = RefdataValue.findByOwnerAndValue(cat, value);
-    }
+//    if ( !result ) {
+//      new RefdataValue(owner:cat, value:value, label:(label?:value)).save(flush:true)
+//      result = RefdataValue.findByOwnerAndValue(cat, value)
+//    }
 
     result
   }
--- service/grails-app/domain/org/olf/general/RefdataValue.groovy
+++ service/grails-app/domain/org/olf/general/RefdataValue.groovy
@@ -7,27 +7,21 @@ class RefdataValue implements MultiTenant<RefdataValue> {
   String id
   String value
   String label
-  String style
 
   static belongsTo = [
     owner:RefdataCategory
   ]
-  
-  static hasMany = [
-  ]
 
   static mapping = {
-              id column: 'rdv_id', generator: 'uuid', length:36
-         version column: 'rdv_version'
-           owner column: 'rdv_owner', index:'rdv_entry_idx'
-           value column: 'rdv_value', index:'rdv_entry_idx'
-           label column: 'rdv_label'
-           style column: 'rdv_style'
+    id column: 'rdv_id', generator: 'uuid', length:36
+    version column: 'rdv_version'
+    owner column: 'rdv_owner', index:'rdv_entry_idx'
+    value column: 'rdv_value', index:'rdv_entry_idx'
+    label column: 'rdv_label'
   }
 
   static constraints = {
-           style(nullable: true, blank: false)
-           label(nullable: true, blank: false)
+    label(nullable: true, blank: false)
   }
 
 }
