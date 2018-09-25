e22645af18357f9935458b6ad913a86ff65e0289
Ian Ibbotson <ian.ibbotson@k-int.com>
2018-09-25 10:09:19 +0100

    Refactor eResources to include titles and packages instead of platform titles and packages

--- service/grails-app/domain/org/olf/kb/ElectronicResource.groovy
+++ service/grails-app/domain/org/olf/kb/ElectronicResource.groovy
@@ -14,8 +14,7 @@ public class ElectronicResource implements MultiTenant<ElectronicResource> {
  
   String id
   String type
-  Pkg pkg
-  PlatformTitleInstance pti
+  TitleInstance ti
   String name
   Platform plat
 
@@ -24,8 +23,7 @@ public class ElectronicResource implements MultiTenant<ElectronicResource> {
     version false
          id generator: 'assigned', column:'id'
        type column:'type'
-        pkg column:'pkg_id'
-        pti column:'pti_id'
+         ti column:'ti_id'
        name column:'name'
        plat column:'plat_fk'
   }
