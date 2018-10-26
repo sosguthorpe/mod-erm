5dc9ff664b2cc35b55732f4a18d784a84b9ff2c2
Mark Deutsch <markpd@gmail.com>
2018-10-26 04:28:43 -0400

    ErmResource: Added defaults for 'type' RDV (#46)

--- service/grails-app/domain/org/olf/kb/ErmResource.groovy
+++ service/grails-app/domain/org/olf/kb/ErmResource.groovy
@@ -2,6 +2,7 @@ package org.olf.kb
 
 import org.olf.erm.Entitlement
 import org.olf.general.RefdataValue
+import org.olf.general.refdata.Defaults
 
 import grails.gorm.MultiTenant
 
@@ -9,13 +10,14 @@ import grails.gorm.MultiTenant
  * an ErmResource - Superclass
  */
 public class ErmResource implements MultiTenant<ErmResource> {
- 
+
   String id
   String name
-  
+
+  @Defaults(['Audio', 'Book', 'Database', 'Film', 'Image', 'Journal'])
   RefdataValue type
   RefdataValue subType
-  
+
   static hasMany = [
     entitlements: Entitlement
   ]
@@ -36,9 +38,9 @@ public class ErmResource implements MultiTenant<ErmResource> {
             type (nullable:true, blank:false)
          subType (nullable:true, blank:false)
   }
-  
+
   String toString() {
     name
   }
-   
+
 }
