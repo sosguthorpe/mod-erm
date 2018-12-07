4e414e3798e84569f1810ce31a4155b318153468
Ian Ibbotson <ianibbo@googlemail.com>
2018-12-07 07:58:36 +0000

    Tidy register_and_enable script

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -11,11 +11,11 @@ if [ ! -d "$DESCRIPTORDIR" ]; then
     ./gradlew generateDescriptors
 fi
 
-curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/olf-erm-1.0.0
-curl -XDELETE http://localhost:9130/_/discovery/modules/olf-erm-1.0.0/localhost-olf-erm-1.0.0
-curl -XDELETE http://localhost:9130/_/proxy/modules/olf-erm-1.0.0
+curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/olf-erm-1.0.3
+curl -XDELETE http://localhost:9130/_/discovery/modules/olf-erm-1.0.0/localhost-olf-erm-1.0.3
+curl -XDELETE http://localhost:9130/_/proxy/modules/olf-erm-1.0.3
 # ./gradlew clean generateDescriptors
 curl -XPOST http://localhost:9130/_/proxy/modules -d @"${DESCRIPTORDIR}/ModuleDescriptor.json"
 curl -XPOST http://localhost:9130/_/discovery/modules -d @"${DESCRIPTORDIR}/DeploymentDescriptor.json"
-curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "olf-erm-1.0.0"}'
+curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "olf-erm-1.0.3"}'
 popd
