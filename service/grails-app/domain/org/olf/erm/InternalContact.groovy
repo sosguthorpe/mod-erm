0432a7012c1126b8afe9a2df98e319970541153a
Ian Ibbotson <ianibbo@googlemail.com>
2018-12-12 11:11:24 +0000

    Tidy up internal contact domain class

--- service/grails-app/domain/org/olf/erm/InternalContact.groovy
+++ service/grails-app/domain/org/olf/erm/InternalContact.groovy
@@ -5,28 +5,28 @@ import org.olf.general.refdata.Defaults
 import grails.gorm.MultiTenant
 
 public class InternalContact implements MultiTenant<InternalContact>{
-	
-	String id
-	String user
-	@Defaults(['Agreement owner', 'Subject specialist']) // Defaults to create for this property.
-	RefdataValue role
-	
-	static belongsTo = [
-		owner: SubscriptionAgreement
-	]
+  
+  String id
+  String user
+  @Defaults(['Agreement owner', 'Subject specialist', 'General Contact']) // Defaults to create for this property.
+  RefdataValue role
+  
+  static belongsTo = [
+    owner: SubscriptionAgreement
+  ]
 
     static mapping = {
-//		table 'internal_contact'
+//    table 'internal_contact'
                    id column: 'ic_id', generator: 'uuid', length:36
               version column: 'ic_version'
                 owner column: 'ic_owner_fk'
-				 user column: 'ic_user_fk'
-			     role column: 'ic_role'
+                 user column: 'ic_user_fk'
+                 role column: 'ic_role'
   }
 
   static constraints = {
-	  owner(nullable:false, blank:false);
-	  user(nullable:true, blank:false);
-	  role(nullable:true, blank:false);
+       owner(nullable:false, blank:false);
+        user(nullable:true, blank:false);
+        role(nullable:true, blank:false);
   }
 }
