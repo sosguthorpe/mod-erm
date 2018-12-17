31a6d5e3705d50ada19d720af5a8c6eed6ae2750
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-12-17 20:10:47 +0000

    Fixed entry point for the /sas/{id}/resources

--- service/grails-app/controllers/org/olf/SubscriptionAgreementController.groovy
+++ service/grails-app/controllers/org/olf/SubscriptionAgreementController.groovy
@@ -1,16 +1,15 @@
 package org.olf
 
-import grails.gorm.multitenancy.CurrentTenant
-import groovy.util.logging.Slf4j
-import com.k_int.okapi.OkapiTenantAwareController
+import org.hibernate.sql.JoinType
 import org.olf.erm.SubscriptionAgreement
-import org.olf.erm.SubscriptionAgreementDataService
-import org.olf.erm.SubscriptionAgreementService
-import org.olf.erm.Entitlement
-import org.olf.kb.Pkg
+import org.olf.kb.ErmResource
 import org.olf.kb.PackageContentItem
 import org.olf.kb.PlatformTitleInstance
-import grails.converters.JSON
+
+import com.k_int.okapi.OkapiTenantAwareController
+
+import grails.gorm.multitenancy.CurrentTenant
+import groovy.util.logging.Slf4j
 
 
 /**
@@ -22,15 +21,28 @@ import grails.converters.JSON
 @CurrentTenant
 class SubscriptionAgreementController extends OkapiTenantAwareController<SubscriptionAgreement>  {
   
-  // Data service
-  SubscriptionAgreementDataService subscriptionAgreementDataService
-
   SubscriptionAgreementController() {
     super(SubscriptionAgreement)
   }
   
   def resources (String subscriptionAgreementId) {
-    subscriptionAgreementService.resourcesFor (subscriptionAgreementId)
+    if (subscriptionAgreementId) {
+      
+      respond doTheLookup (ErmResource) {
+        or {
+          // Resources linked via a package.
+          createAlias 'pkg', 'pci_pkg', JoinType.LEFT_OUTER_JOIN
+            createAlias 'pci_pkg.entitlements', 'pci_pkg_ent', JoinType.LEFT_OUTER_JOIN
+              eq 'pci_pkg_ent.owner.id', subscriptionAgreementId
+          
+          // Ptis linked explicitly.
+          createAlias 'entitlements', 'pti_ent', JoinType.LEFT_OUTER_JOIN
+            eq 'pti_ent.owner.id', subscriptionAgreementId    
+        }
+      }
+    } else {
+      respond (params.boolean('stats') ? Collections.EMPTY_MAP : Collections.EMPTY_SET)
+    }
   }
 }
 
