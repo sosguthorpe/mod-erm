4b01849de7032568bbce53c7149107c7b9b76c70
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-12-17 14:38:59 +0000

    OKAPI module should provide okapi mappings.

--- service/grails-app/controllers/org/olf/UrlMappings.groovy
+++ service/grails-app/controllers/org/olf/UrlMappings.groovy
@@ -6,7 +6,6 @@ class UrlMappings {
   static mappings = {
 
     "/"(controller: 'application', action:'index')
-    "/_/tenant"(controller: 'okapi', action:'tenant')
     "500"(view: '/error')
     "404"(view: '/notFound')
 
