19b9e1d3386e162911b49f46dde02e61706ca878
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-12-17 14:51:14 +0000

    The startup should now be handled correctly by the modules.

--- service/grails-app/init/org/olf/Application.groovy
+++ service/grails-app/init/org/olf/Application.groovy
@@ -17,29 +17,4 @@ class Application extends GrailsAutoConfiguration {
   static void main(String[] args) {
     GrailsApp.run(Application, args)
   }
-  
-  @Override
-  void doWithDynamicMethods() {
-    // Bind extra methods to the class.
-    log.debug("Extending Domain classes.")
-    (grailsApplication.getArtefacts("Domain")).each {GrailsClass gc ->
-      GrailsDomainRefdataHelpers.addMethods(gc)
-    }
-  }
-  
-  @Override
-  void onStartup(Map<String, Object> event) {
-    // Ensure the defaults.
-    OkapiTenantAdminService okapiTenantAdminService = grailsApplication.mainContext.getBean(OkapiTenantAdminService)
-    
-    // Grails uses the schema id as the internal tenant ID
-    okapiTenantAdminService.allTenantSchemaIds.each { Serializable tid ->
-      GrailsDomainRefdataHelpers.setDefaultsForTenant( tid )
-    }
-  }
-  
-  @Bean
-  UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory(){
-      new UndertowEmbeddedServletContainerFactory()
-  }
 }
