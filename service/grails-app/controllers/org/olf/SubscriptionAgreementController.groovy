148309d2401870f94940e41e23ab8eea7401bb5d
Ian Ibbotson <ianibbo@googlemail.com>
2019-02-01 13:58:49 +0000

    Working on variants for listing entitlement titles

--- service/grails-app/controllers/org/olf/SubscriptionAgreementController.groovy
+++ service/grails-app/controllers/org/olf/SubscriptionAgreementController.groovy
@@ -5,6 +5,7 @@ import org.olf.erm.SubscriptionAgreement
 import org.olf.kb.ErmResource
 import org.olf.kb.PackageContentItem
 import org.olf.kb.PlatformTitleInstance
+import org.olf.kb.Pkg
 import org.olf.erm.Entitlement
 
 import com.k_int.okapi.OkapiTenantAwareController
@@ -76,41 +77,41 @@ class SubscriptionAgreementController extends OkapiTenantAwareController<Subscri
   }
 
   def resources2(String subscriptionAgreementId) {
-    if (SubscriptionAgreement.read(subscriptionAgreementId)?.items?.size() ?: 0 > 0) {
 
-        DetachedCriteria resources_with_an_entitlement = ErmResource.where {
+    // String query = '''
+    //   select r from ErmResource as r 
+    //   where exists ( Select e from Entitlement as e where e.resource.id = r.id )
+    //      or exists ( Select pci 
+    //                  from PackageContentItem as pci 
+    //                  where pci.pti.id = r.id 
+    //                    and exists ( select e from Entitlement as e where r.resource.id = pci.pkg.id ) )
+    // '''
+    // select sa, e, e.resource, nvl(pci, e.resource)
+    // from SubscriptionAgreement as sa join sa.entitlements as e left outer join PackageContentItem as pci on pci.pkg = e.resource
 
-          def res = ErmResource
-
-          // Where there is an entitlement directly for this resource
-          exists Entitlement.where {
-            def e1 = Entitlement
-            def r = resource
-            // e1.resource is a property on Entitlement, res is the outer resource
-            return r.id == res.id
-          }.id()
-          // || exists Entitlement.where {
-          //   // OR there is an entitlement for a package that contais this resource.
-          //   Entitlement e2
-          //   // e1.resource is a property on Entitlement, res is the outer resource
-          //   e2.resource.id == res.id
-          // }
-        }
-
-        println("number of resources with an entitlement ${resources_with_an_entitlement.list().size()}");
-
-        DetachedCriteria resources_with_an_entitlement_2 = ErmResource.where {
-          id == 'hello'
-        }
-
-        println("number of resources with an entitlement_2 ${resources_with_an_entitlement_2.list().size()}");
-
-        respond doTheLookup (ErmResource) { resources_with_an_entitlement_2 }
+    DetachedCriteria resources_with_an_entitlement = ErmResource.where {
+      def res = ErmResource
+      // Where there is an entitlement directly for this resource
+      exists Entitlement.where {
+        def e1 = Entitlement
+        def r = resource
+        r.id == res.id
+        // || exists PackageContentItem.where {
+        //      def pci = PackageContentItem
+        //      return ( pci.pkg.id == r.id && pci.pti.id == res.id )
+        // }
+      } 
     }
 
-    return
-    // If not matched above return with empty collection...
-    respond (params.boolean('stats') ? Collections.EMPTY_MAP : Collections.EMPTY_SET)
+    println("number of resources with an entitlement ${resources_with_an_entitlement.list().size()}");
+
+    DetachedCriteria resources_with_an_entitlement_2 = ErmResource.where {
+      id == 'hello'
+    }
+    println("number of resources with an entitlement_2 ${resources_with_an_entitlement_2.list().size()}");
+
+    respond doTheLookup (ErmResource) { resources_with_an_entitlement_2 }
+
   }
 }
 
