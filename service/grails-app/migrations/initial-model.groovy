98a7d126d7810ff81748f4affa2b0e1fcad5d5e5
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-02-28 16:39:36 +0100

    ERM-46: add String licenseNote to SubscriptionAgreement add column sa_license_note to initial-model.groovy

--- service/grails-app/migrations/initial-model.groovy
+++ service/grails-app/migrations/initial-model.groovy
@@ -693,6 +693,8 @@ databaseChangeLog = {
             column(name: "sa_enabled", type: "BOOLEAN")
 
             column(name: "sa_description", type: "VARCHAR(255)")
+
+            column(name: "sa_license_note", type: "VARCHAR(255)")
         }
     }
 
