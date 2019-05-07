c8b29aa474ce048f7ce73d8a72f0b6b0fb633137
Ian Ibbotson <ianibbo@googlemail.com>
2019-05-07 17:42:31 +0100

    working on updates to org for ERM-220

--- service/grails-app/domain/org/olf/general/Org.groovy
+++ service/grails-app/domain/org/olf/general/Org.groovy
@@ -8,8 +8,7 @@ class Org implements MultiTenant<Org> {
 
   String id
   String name
-  String vendorsUuid
-  String reference
+  String orgsUuid
 
   // IF this resource is controlled by another service,
   // what is the URI of the primary resource
@@ -20,16 +19,12 @@ class Org implements MultiTenant<Org> {
             id column: 'org_id', generator: 'uuid2', length:36
        version column: 'org_version'
           name column: 'org_name'
-   vendorsUuid column: 'org_vendors_uuid'
-     sourceURI column: 'org_source_uri'
-     reference column: 'org_reference'
+      orgsUuid column: 'org_orgs_uuid'
   }
 
   static constraints = {
-      sourceURI(nullable:true, blank:false)
-    vendorsUuid(nullable:true, blank:false)
-      reference(nullable:true, blank:false)
-
+           name(nullable:true, blank:false)
+       orgsUuid(nullable:true, blank:false)
   }
 
   /**
