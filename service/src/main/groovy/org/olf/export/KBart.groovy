e12384002a4314c8e0862db6af5ee9511e4a6024
John Fereira <jaf30@cornell.edu>
2019-08-05 12:15:41 -0400

    Erm 215 (#153)
    * initial commit for ERM-265
    
    * add ExportController for ERM-265
    
    * initial commit for ERM-265
    
    * add first line indicating this is a bash script
    
    * add titles.sh script for testing titles export
    
    * swap code in index and kbart methods
    
    * changed kbart method to "titles" and use TitleInstance.entitiled to retrieve list
    
    * add stats to end of endpoint
    
    * call exportService.entitiled() instead of titles method
    
    * changes to  make subscriptionAgreementId optional
    
    * initial implementation of entitled method
    
    * test for exportController to add some fields
    
    * change coverageStatemens to coverage, to expand coverage correctly
    
    * initial commit of view for exports
    
    * add verbose option
    
    * add script for testing export for a specific agreement
    
    * for kbart header, call KBart.asMap() to get fields using reflection
    
    * add route for export of specific agreement as /erm/sas/$id/export/$format
    
    * add code to test for agreement id.  Add initial mapToKbart method
    
    * initial version for kbart export
    
    * add view for export entries
    
    * chang toString method to separate fields with tabs
    
    * remove extraneous export
    
    * replace with Steves version
    
    * pull mapToKbart into exportservice
    
    * update entitled method with new query from Steve
    
    * added platform, url, depth, note ... but they're not rendering
    
    * fix platform, url, depth, note variables
    
    * grab paramater so that script can be used for one or all agreements
    
    * pass agreementId to exportService
    
    * remove work and platformInstances elements
    
    * pick up agreementId to support ERM-327 and ERM-328..still a WIP
    
    * grab first param to support single agreement lookup
    
    * removed
    
    * add HQL which returns runs for specific agreementId...not yet working
    
    * make action optional for /erm/export path
    
    * remove extraneous if (agreementId) test, clean up indenting
    
    * fix indenting
    
    * add opencsv4.6 library
    
    * add unimplemented fields as empty strings to align content with header
    
    * fix url, note, and depth fields
    
    * clean up indenting and add coverage statements
    
    * use opencsv write to outputstream
    
    * clean up formatting and use ICVSWriter to a buffered stream for output
    
    * add CSV binding annotation to order results when serialized. Add static header method to return fields as a String array
    
    * Use StatefulBeanToCSV class to serialize list of KBart objects.  Move mapping and header methods to KBart class
    
    * a little code cleanup

--- service/src/main/groovy/org/olf/export/KBart.groovy
+++ service/src/main/groovy/org/olf/export/KBart.groovy
@@ -1,63 +1,269 @@
 package org.olf.export
 
+import groovy.transform.AutoClone
 import java.lang.reflect.Field
+import groovy.util.logging.Slf4j
+import com.opencsv.bean.CsvBindByPosition
 
-public class KBart {
+import org.olf.erm.Entitlement
+import org.olf.kb.ErmResource
+import org.olf.kb.PackageContentItem
+import org.olf.kb.Pkg
+import org.olf.kb.TitleInstance
+import org.olf.kb.PlatformTitleInstance
+import org.olf.kb.IdentifierOccurrence
+import org.olf.kb.Identifier
+import org.olf.kb.IdentifierNamespace
+import org.olf.kb.CoverageStatement
+import org.olf.kb.Platform
+
+@AutoClone(excludes = ['date_first_issue_online', 'date_last_issue_online'])
+public class KBart implements Serializable {
 	
-   KBart() {
-		
-   }
-
-    public String title_id = "" // not implemented
-    public String publication_title = "" // TitleInstance.name
-    public String title_url = "" // platformTitleInstance.url
-    public String print_identifier = "" // titleInstance.identifiers.value WHERE .ns = "issn" OR ??? WHERE .ns = "isbn"
-    public String online_identifier = "" // titleInstance.identifiers.value WHERE .ns = "eissn"
-    public String date_first_issue_online = "" // derived from coverageService
-    public String num_first_vol_online = "" // not implemented
-    public String num_first_issue_online = "" // not implemented
-    public String date_last_issue_online = "" // derived from coverageService
-    public String num_last_vol_online = "" // not implemented
-    public String num_last_issue_online = "" // not implemented
+  KBart() {
+	//	
+  }
+  @CsvBindByPosition(position = 0)
+  public String title_id = "" // not implemented
+  @CsvBindByPosition(position = 1)
+  public String publication_title = "" // TitleInstance.name
+  @CsvBindByPosition(position = 2)
+  public String title_url = "" // platformTitleInstance.url
+  @CsvBindByPosition(position = 3)
+  public String print_identifier = "" // titleInstance.identifiers.value WHERE .ns = "issn" OR ??? WHERE .ns = "isbn"
+  @CsvBindByPosition(position = 4)
+  public String online_identifier = "" // titleInstance.identifiers.value WHERE .ns = "eissn"
+  @CsvBindByPosition(position = 5)
+  public String date_first_issue_online = "" // derived from coverageService
+  @CsvBindByPosition(position = 6)
+  public String num_first_vol_online = "" // not implemented
+  @CsvBindByPosition(position = 7)
+  public String num_first_issue_online = "" // not implemented
+  @CsvBindByPosition(position = 8)
+  public String date_last_issue_online = "" // derived from coverageService
+  @CsvBindByPosition(position = 9)
+  public String num_last_vol_online = "" // not implemented
+  @CsvBindByPosition(position = 10)
+  public String num_last_issue_online = "" // not implemented
+  @CsvBindByPosition(position = 11)
+  public String first_author = "" // not implemented
+  @CsvBindByPosition(position = 12)
+  public String embargo_info = "" // not implemented
+  @CsvBindByPosition(position = 13)
+  public String coverage_depth = "" // platformContentItem.depth
+  @CsvBindByPosition(position = 14)
+  public String notes = "" // platformContentItem.note
+  @CsvBindByPosition(position = 15)
+  public String publisher_name = "" // ????
+  @CsvBindByPosition(position = 16)
+  public String publication_type  = "" // TitleInstance.type
+  @CsvBindByPosition(position = 17)
+  public String date_monograph_published_print = "" // not implemented
+  @CsvBindByPosition(position = 18)
+  public String date_monograph_published_online = "" // not implemented
+  @CsvBindByPosition(position = 19)
+  public String monograph_volume = "" // not implemented
+  @CsvBindByPosition(position = 20)
+  public String monograph_edition = "" // not implemented
+  @CsvBindByPosition(position = 21)
+  public String first_editor = "" // not implemented
+  @CsvBindByPosition(position = 22)
+  public String parent_publication_title_id = "" // not implemented
+  @CsvBindByPosition(position = 23)
+  public String preceding_publication_title_id = "" // not implemented  
+  @CsvBindByPosition(position = 24)
+  public String access_type = "" // not implemented
+ 
+  // public get methods are required for csv serialization
+  
+  public String getTitle_id() {
+	  return this.title_id
+  }
+  
+  public String getPublication_title() {
+	  return this.publication_title
+  }
+  
+  public String getTitle_url() {
+	  return this.title_url
+  }
+  
+  public String getPrint_identifier() {
+	  return this.print_identifier
+  }
+  
+  public String getOnline_identifier() {
+	  return this.online_identifier
+  }
+  
+  public String getDate_first_issue_online() {
+	  return this.date_first_issue_online
+  }
+  
+  public String getNum_first_vol_online() {
+	  return this.num_first_vol_online
+  }
+  
+  public String getNum_first_issue_online() {
+	  return this.num_first_issue_online
+  }
+  
+  
+  public String getDate_last_issue_online() {
+	  return this.date_last_issue_online
+  }
+  
+  public String getNum_last_vol_online() {
+	  return this.num_last_vol_online
+  }
+  
+  public String getNum_last_issue_online() {
+	  return this.num_last_issue_online
+  } 
+  
+  public String getFirst_author() {
+	  return this.first_author
+  }
+  
+  public String getEmbargo_info() {
+	  return this.embargo_info
+  }
+  
+  public String getCoverage_depth() {
+	  return this.coverage_depth
+  }
+  
+  public String getNotes() {
+	  return this.notes
+  }
+  
+  public String getPublisher_name() {
+	  return this.publisher_name
+  }
+  
+  public String getPublication_type() {
+	  return this.publication_type
+  }
+  
+  
+  public String getDate_monograph_published_print() {
+	  return this.date_monograph_published_print
+  }
    
-    public String first_author = "" // not implemented
-    public String embargo_info = "" // not implemented
-    public String coverage_depth = "" // platformContentItem.depth
-    public String notes = "" // platformContentItem.note
-    public String publisher_name = "" // ????
-    public String publication_type  = "" // TitleInstance.type
-    public String date_monograph_published_print = "" // not implemented
-    public String date_monograph_published_online = "" // not implemented
-    public String monograph_volume = "" // not implemented
-    public String monograph_edition = "" // not implemented
-    public String first_editor = "" // not implemented
-    public String parent_publication_title_id = "" // not implemented
-    public String preceding_publication_title_id = "" // not implemented
-    public String access_type = "" // not implemented
-	
-	
-	
-	public String toString() {
-		final String separator = "\t"
-		String s = ""+
-		this.publication_title + separator +
-		this.online_identifier + separator +
-		this.print_identifier + separator +
-		this.date_first_issue_online + separator +
-		this.date_last_issue_online + separator +
-		this.title_url + separator +
-		this.coverage_depth + separator +
-		this.notes + separator +
-		this.publication_type 
-		return s
+  public String getDate_monograph_published_online() {
+	  return this.date_monograph_published_online
+  }
+  
+  public String getMonograph_volume() {
+	  return this.monograph_volume
+  }
+  
+  public String getMonograph_edition() {
+	  return this.monograph_edition
+  }
+  
+  public String getFirst_editor() {
+	  return this.first_editor
+  }
+  
+  public String getParent_publication_title_id() {
+	  return this.parent_publication_title_id
+  }
+  
+  public String getPreceding_publication_title_id() {
+	  return this.preceding_publication_title_id
+  }
+  
+  public String getAccess_type() {
+	  return this.access_type
+  }
+  
+  static String[] header() {
+    List<String> header = new ArrayList<String>()
+	KBart.class.getDeclaredFields().each {
+	  if (it.modifiers == java.lang.reflect.Modifier.PUBLIC) {
+		header.add(it.name)
+	  }
 	}
+	return header as String[] 
+  }
+   
+    
+  /*
+   * Still a WIP.  Needs processing of obj[1] and obj[2]. For now, test data does not provide direct
+   * Entitlements and no additional info for kbart export is available
+   */
+  static List<KBart> transform(final List<Object> objects) {
+	 	  
+	List<KBart> kbartList = new ArrayList<KBart>();
 	
-	 
-	
-	public Map asMap() {
-		this.class.declaredFields.findAll { !it.synthetic }.collectEntries {
-		  [ (it.name):this."$it.name" ]
-		}
-	}
+	int resnum = 0;
+	for (Object obj: objects) {
+	  resnum++
+	  KBart kbart = new KBart()
+	  ErmResource res  = (ErmResource) obj[0]
+	  //if (obj[1] != null) log.debug("obj[1] class: "+ obj[1].getClass().getName())
+	  //if (obj[2] != null) log.debug("obj[2] class: "+ obj[2].getClass().getName())
+	  
+	  if (obj[0] instanceof PackageContentItem) {
+		PackageContentItem pci = (PackageContentItem) obj[0]
+		PlatformTitleInstance pti = pci.pti
+		TitleInstance ti = pti.titleInstance
 
+		kbart.publication_title = ti.name
+		kbart.publication_type = ti.type.value
+		if (pci.depth) kbart.coverage_depth = pci.depth
+		if (pci.note) kbart.notes = pci.note
+		if (pti.url) kbart.title_url = pti.url
+		
+		Object identifiers_obj = ti.identifiers
+		if (identifiers_obj) {
+		  Iterator iter = ti.identifiers.iterator();
+		  while (iter.hasNext()) {
+			IdentifierOccurrence thisIdent = iter.next()
+			Identifier ident =  thisIdent.identifier
+			if (ident) {
+			  if (ident.ns.value.equals("eissn")) {
+				kbart.online_identifier = ident.value
+			  } else if (ident.ns.value.equals("isbn")) {
+				kbart.print_identifier = ident.value
+			  } else if (ident.ns.value.equals("issn")) {
+				kbart.print_identifier = ident.value
+			  }
+			}
+		  }
+		}
+		// get coverage statements. if there is more than one we need to clone the kbart object, one for
+		// each coverage statement
+		Set<CoverageStatement> coverages = ti.coverage
+		// log.debug("coverage size: "+ coverages.size())
+		// add one kbart object for each coverage to list
+		coverages.each { coverage ->
+		  //if (coverages.size() > 1 ) log.debug(resnum + ': title: '+ ti.name +' startDate: '+ coverage.startDate +" - endDate: "+ coverage.endDate )
+		  KBart kbartclone = new KBart();
+		  kbartclone = kbart.clone()
+		  kbartclone.date_first_issue_online = coverage.startDate.toString()
+		  kbartclone.date_last_issue_online = coverage.endDate.toString()
+		  kbartList.add(kbartclone)
+		}
+	  }
+	  
+	  // deal with 2nd and 3rd resources in the result.
+	  
+	  /*if (obj[1] instanceof Entitlement) { 
+		Entitlement ent = (Entitlement) obj[1];
+		ErmResource resource = ent.resource 
+	  }*/
+	  
+	  /*
+	  if (obj[2] instanceof Entitlement) {
+		log.debug(resnum +": got entitlement in obj2")
+		Entitlement ent = (Entitlement) obj[2];
+		ErmResource resource = ent.resource
+	  }*/
+	}
+	// log.debug("returning list of kbart objects")
+	return kbartList
+  }
 }
+
