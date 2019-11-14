a6378489ab5072304a2aeb39303c262655594999
sosguthorpe <steve.osguthorpe@k-int.com>
2019-11-14 11:28:31 +0000

    Order lines now an object. (#205)

--- service/grails-app/domain/org/olf/erm/OrderLine.groovy
+++ service/grails-app/domain/org/olf/erm/OrderLine.groovy
@@ -1,8 +1,10 @@
 package org.olf.erm;
 
+import com.k_int.web.toolkit.domain.traits.Clonable
+
 import grails.gorm.MultiTenant
 
-public class OrderLine implements MultiTenant<OrderLine> {
+public class OrderLine implements MultiTenant<OrderLine>, Clonable<OrderLine> {
 	
 	String id
 	String poLineId
@@ -15,11 +17,18 @@ public class OrderLine implements MultiTenant<OrderLine> {
 				version column: 'pol_version'
 				  owner column: 'pol_owner_fk'  
 			   poLineId column: 'pol_orders_fk'
-				   
 	}
   
 	static constraints = {
 		 owner(nullable:false, blank:false);
 	   poLineId(nullable:true, blank:false);
-	}  
+	}
+  
+  /**
+   * Need to resolve the conflict manually and add the call to the clonable method here.
+   */
+  @Override
+  public OrderLine clone () {
+    Clonable.super.clone()
+  }
 }
