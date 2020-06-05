4e11a281e95916be25f78dae2bd3ca06c1f58989
Peter Boehm <boehm@hebis.uni-frankfurt.de>
2020-06-05 17:28:25 +0200

    ERM-510

--- service/grails-app/services/org/olf/general/jobs/JobLoggingService.groovy
+++ service/grails-app/services/org/olf/general/jobs/JobLoggingService.groovy
@@ -12,35 +12,36 @@ import org.grails.async.factory.future.CachedThreadPoolPromiseFactory
 
 @Slf4j
 class JobLoggingService {
-  
+
   static {
     promiseFactory = new CachedThreadPoolPromiseFactory (10, 5L, TimeUnit.SECONDS)
   }
-  
+
   @Subscriber('jobs:log_error')
   void handleLogError(final String tenantId, final String jobId, final String message) {
     handleLogEvent(tenantId, jobId, message, LogEntry.TYPE_ERROR)
   }
-  
+
   @Subscriber('jobs:log_info')
   void handleLogInfo (final String tenantId, final String jobId, final String message) {
     handleLogEvent(tenantId, jobId, message, LogEntry.TYPE_INFO)
   }
-  
+
   private final static Closure addLogEntry = { final Map<String, ?> logProperties, final Serializable jobId ->
     LogEntry le = new LogEntry(logProperties)
     le.save(failOnError: true, flush: true)
   }
-  
+
   static void handleLogEvent ( final String tenantId, final String jobId, final String message, final String type, final Instant timestamp = Instant.now(), final Map<String, String> contextVals = null) {
     Promise p = task {
       final Map<String, ?> jobProperties = [
         'type': type,
         'origin': jobId,
         'message': message,
-        'dateCreated': timestamp
+        'dateCreated': timestamp,
+        'additionalinfo': contextVals
       ]
-      
+
       if ( jobId ) {
         if (tenantId) {
           Tenants.withId( tenantId, addLogEntry.curry(jobProperties, jobId) )
