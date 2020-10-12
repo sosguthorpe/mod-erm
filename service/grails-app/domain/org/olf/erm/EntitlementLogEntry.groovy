c3ebd9eff3cce3fb861c4a561913b2ae6c3f5799
Ian Ibbotson <ianibbo@gmail.com>
2020-10-12 19:41:26 +0100

    Add EntitlementLogEntry and mapping

---
+++ service/grails-app/domain/org/olf/erm/EntitlementLogEntry.groovy
@@ -0,0 +1,44 @@
+package org.olf.erm
+
+import java.time.LocalDate
+import grails.compiler.GrailsCompileStatic
+import grails.gorm.MultiTenant
+import groovy.util.logging.Slf4j
+import org.olf.kb.ErmResource
+
+@Slf4j
+@GrailsCompileStatic
+class EntitlementLogEntry implements MultiTenant<EntitlementLogEntry>  {
+  
+  String id
+  String seqid
+  LocalDate startDate
+  LocalDate endDate
+  ErmResource res
+  Entitlement directEntitlement
+  Entitlement packageEntitlement
+  
+
+  static belongsTo = [
+  ]
+  
+  static mapping = {
+                      id column:'ele_id', generator: 'uuid2', length:36
+                   seqid column:'ele_seq_id'
+               startDate column:'ele_start_date'
+                 endDate column:'ele_end_date'
+                     res column:'ele_res'
+       directEntitlement column:'ele_direct_entitlement'
+      packageEntitlement column:'ele_pkg_entitlement'
+  }
+   
+  static constraints = {
+                    seqid(nullable:false)
+                startDate(nullable:false)
+                  endDate(nullable:true)
+                      res(nullable:false)
+        directEntitlement(nullable:true)
+       packageEntitlement(nullable:true)
+  }
+  
+}
