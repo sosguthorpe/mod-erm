893479de3b415bd50f7929a7d3394cceb5a85192
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-10-22 16:30:02 +0100

    Added methods to perform string templating by context

--- service/grails-app/services/org/olf/general/StringTemplatingService.groovy
+++ service/grails-app/services/org/olf/general/StringTemplatingService.groovy
@@ -6,12 +6,10 @@ public class StringTemplatingService {
 
   /*
    * This method will take in an id of the form f311d130-8024-47c4-8a86-58f817dbefde
-   * It will return a Map of StringTemplates grouped by context, that are relevant for this id 
+   * It will return a Map of StringTemplates grouped by context, that are relevant for this id
   */
-  Map<String, Set<StringTemplate>> findStringTemplatesForId(String id) {
+  public Map<String, Set<StringTemplate>> findStringTemplatesForId(String id) {
     Map templates = [:]
-
-    println("findStringTemplatesForId called with id (${id})")
     // Below we build up the relevant StringTemplates scope by scope
 
     // urlProxiers treat the StringTemplate's idScopes as a blacklist,  use queryNotInScope...
@@ -21,6 +19,59 @@ public class StringTemplatingService {
     templates.urlCustomisers = queryInScopeWithContext(id, 'urlcustomiser')
 
     return templates
+  }
+
+  /*
+   * This method will take in an id of the form f311d130-8024-47c4-8a86-58f817dbefde, and a binding of the form
+  [
+    inputUrl: "http://ebooks.ciando.com/book/index.cfm?bok_id=27955222",
+    platformLocalCode: "ciando"
+  ]
+   * and these will be accessible from the template rule.
+   * This method will store the business logic determining heirachy of StringTemplate contexts,
+   * and whether these "stack" or not.
+   */
+  public Map performStringTemplates(String id, Map binding) {
+    Map output = [
+      url: binding.inputUrl
+    ]
+
+    Map stringTemplates = findStringTemplatesForId(id)
+
+    // First get all customised urls
+    output.customisedUrls = performTemplatingByContext(binding, "urlCustomisers", stringTemplates)
+
+    // Then proxy all urls--first the default one
+    output.proxiedUrls = performTemplatingByContext(binding, "urlProxiers", stringTemplates)
+    
+    //Then proxy each customised url
+    output.customisedUrls.collect { customiserMap ->
+      /*
+       customiserMap = [
+         name: "customiserName"
+         value: "customisedUrl"
+       ]
+      */
+      // Think we only need a shallow copy here
+      Map customBinding = binding.clone()
+      customBinding.inputUrl = customiserMap.value
+
+      customiserMap.proxiedUrls = performTemplatingByContext(customBinding, "urlProxiers", stringTemplates)
+      return customiserMap
+    } 
+
+    return output
+  }
+
+
+  // Simpler method which just returns a list of maps for a single StringTemplateContext--used for nested templating
+  private ArrayList<Map> performTemplatingByContext(Map binding, String context, Map stringTemplates) {
+    return stringTemplates[context]?.collect { st ->
+      [
+        name: st.name,
+        value: st.customiseString(binding)
+      ]
+    }
   }
 
   private Set<StringTemplate> queryNotInScopeWithContext(String id, String context) {
