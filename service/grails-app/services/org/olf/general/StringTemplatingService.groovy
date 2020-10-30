f3f5710052bf9006e4a81916d88d3f8ab5f4baf6
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-10-30 14:12:16 +0000

    split generation method into system/platform/pti blocks

--- service/grails-app/services/org/olf/general/StringTemplatingService.groovy
+++ service/grails-app/services/org/olf/general/StringTemplatingService.groovy
@@ -176,34 +176,65 @@ public class StringTemplatingService {
     }
   }
 
+  @CompileStatic(SKIP)
+  private void deleteTemplatedUrlsForPlatform(String platformId) {
+    TemplatedUrl.withNewTransaction {
+      log.debug ("LOGDEBUG TU FOR PLATFORM(${platformId}): ${TemplatedUrl.executeQuery('SELECT tu FROM TemplatedUrl as tu JOIN (tu.resource as pti JOIN pti.platform as p) WHERE p.id = :pId', [pId: platformId])}"
+    }
+  }
+
   /*
-   * Trigger plan - have two mutex booleans, running and locked.
-   * On any action the system undertakes which can change multiple Stringtemplates/Platforms/PTIs,
-   * we MUST fire a "lock" event at the start and an "unlock" at the end.
+   * Trigger plan - have a mutex boolean, running, and a queue.
    *
-   * When we recieve a "refresh" event, we first check if locked. If so we discard it
-   * (knowing that later it will unlock and fire a refrsh anyway)
-   * If not locked we check if running. If so we set runAgain to true.
-   * If neither are true we run the task. Once a task is finished we run again if runAgain true.
+   * When we recieve a "refresh" event, we check if running. If so we add a note to the queue
+   * If neither are true we run the task. Once a task is finished we run again if queue non-empty.
   */
 
-  static boolean running = false;
-  static boolean locked = false;
-  // Since having two full refreshes queued is pointless, we just set a boolean to true
-  static boolean runAgain = false;
+  static boolean running = false
+  private ArrayList<Map<String, String>> taskQueue = new ArrayList<Map<String, String>>()
+
+  /* 
+   * This params map needs to have two/three elements:
+   * context (being 'pti', 'platform' or 'stringTemplate'),
+   * id (only valid if context != stringTemplate)
+   * platformId (only valid if context is PTI)
+   * 
+   * It will only add a params block to the queue if it's relevant to do so, and will remove any it overrules
+   */
+  void addTaskToTaskQueue(Map<String, String> params) {
+    if (params.context == 'stringTemplate') {
+      // If we're going to run a full system refresh we can just clear the current queue and run that instead
+      taskQueue.clear()
+      taskQueue.add(params)
+    } else if (
+      params.context == 'platform' &&
+      !taskQueue.any {it.context == 'stringTemplate'} &&
+      !taskQueue.any {it.id == params.id}
+    ) {
+      // Only bother adding if there's no system refresh/platform refresh for this id
+      // Remove all PTI updates with this id as platformId
+      taskQueue.removeIf {p -> p.platformId == params.id}
+      taskQueue.add(params)
+      
+    } else if (
+      !taskQueue.any {it.context == 'stringTemplate'} &&
+      !taskQueue.any {it.id == params.platformId}
+    ) {
+      // Only bother adding if there's no system refresh/platform refresh for this platformId
+      taskQueue.add(params)
+    }
+    log.debug "LOGDEBUG TASK QUEUE SIZE: ${taskQueue.size()}"
+  }
 
 
-  public void generateTemplatedUrlsForErmResources(final String tenantId) {
+  public void generateTemplatedUrlsForErmResources(final String tenantId, Map<String, String> params = [context: 'stringTemplate']) {
     log.debug "generateTemplatedUrlsForErmResources called"
 
-    // If locked then just ignore
+    // If running then just add to queue
     synchronized ( this ) {
-      if ( locked == true ) {
-        log.debug("StringTemplatingSevice::generateTemplatedUrlsForErmResources locked - return");
-        return
-      } else if (running == true) {
+      if (running == true) {
         log.debug("StringTemplatingSevice::generateTemplatedUrlsForErmResources already running - make note to run again when finished");
-        runAgain = true
+        addTaskToTaskQueue(params)
         return
       } else {
         running = true
@@ -213,63 +244,98 @@ public class StringTemplatingService {
     log.debug "LOGDEBUG TASK START TIME"
     Tenants.withId(tenantId) {
       // Initially we should clear all templated URLS in the system
-      deleteAllTemplatedUrls()
-      
-      /* 
-        * Right now we only scope URL templates to Platforms, so fetch a list of Platforms,
-        * then for each one fetch the stringTemplates and a list of PTIs with that platform,
-        * then perform stringTemplating on each PTI
-        */
+      switch (params.context) {
+        case 'stringTemplate':
+          deleteAllTemplatedUrls()
+          /* 
+          * Right now we only scope URL templates to Platforms, so fetch a list of Platforms,
+          * then for each one fetch the stringTemplates and a list of PTIs with that platform,
+          * then perform stringTemplating on each PTI
+          */
 
-      final int platformBatchSize = 100
-      int platformBatchCount = 0
+          final int platformBatchSize = 100
+          int platformBatchCount = 0
 
-      Platform.withNewTransaction {
-        // Fetch the ids and localCodes for all platforms
-        List<List<String>> platforms = batchFetchPlatforms(platformBatchSize, platformBatchCount)
+          Platform.withNewTransaction {
+            // Fetch the ids and localCodes for all platforms
+            List<List<String>> platforms = batchFetchPlatforms(platformBatchSize, platformBatchCount)
 
-        // This will return [[00998c04-8ab3-49ab-9053-c3e8cff328c2, ciando], [021bfce4-0533-465e-9340-1ceaad2a530f, localCode2], ...]
-        while (platforms && platforms.size() > 0) {
-          platformBatchCount ++
-          platforms.each { platform ->
-            Map stringTemplates = findStringTemplatesForId(platform[0])
-            String platformLocalCode = platform[1]
+            // This will return [[00998c04-8ab3-49ab-9053-c3e8cff328c2, ciando], [021bfce4-0533-465e-9340-1ceaad2a530f, localCode2], ...]
+            while (platforms && platforms.size() > 0) {
+              platformBatchCount ++
+              platforms.each { platform ->
+                Map stringTemplates = findStringTemplatesForId(platform[0])
+                String platformLocalCode = platform[1]
 
-              /* 
-              * Now we have the stringTemplates and platformLocalCode for the platform,
-              * find all PTIs on this platform and remove then re-add the templatedUrls
-              */
+                  /* 
+                  * Now we have the stringTemplates and platformLocalCode for the platform,
+                  * find all PTIs on this platform and remove then re-add the templatedUrls
+                  */
 
-              final int ptiBatchSize = 100
-              int ptiBatchCount = 0
-              List<List<String>> ptis = batchFetchPtis(ptiBatchSize, ptiBatchCount, platform[0])
-              while (ptis && ptis.size() > 0) {
-                ptiBatchCount ++
-                ptis.each { pti ->
-                  generateTemplatedUrlsForPti(pti, stringTemplates, platformLocalCode)
-                }
+                  final int ptiBatchSize = 100
+                  int ptiBatchCount = 0
+                  List<List<String>> ptis = batchFetchPtis(ptiBatchSize, ptiBatchCount, platform[0])
+                  while (ptis && ptis.size() > 0) {
+                    ptiBatchCount ++
+                    ptis.each { pti ->
+                      generateTemplatedUrlsForPti(pti, stringTemplates, platformLocalCode)
+                    }
 
+                    // Next page
+                    ptis = batchFetchPtis(ptiBatchSize, ptiBatchCount, platform[0])
+                  }
                 // Next page
-                ptis = batchFetchPtis(ptiBatchSize, ptiBatchCount, platform[0])
+                platforms = batchFetchPlatforms(platformBatchSize, platformBatchCount)
               }
-            // Next page
-            platforms = batchFetchPlatforms(platformBatchSize, platformBatchCount)
+            }
           }
-        }
+          break;
+        case 'platform':
+          deleteTemplatedUrlsForPlatform(params.id)
+          Platform p = Platform.get(params.id)
+          Map stringTemplates = findStringTemplatesForId(p.id)
+          String platformLocalCode = p.localCode
+          /* 
+          * We have the stringTemplates and platformLocalCode for the platform,
+          * find all PTIs on this platform and remove then re-add the templatedUrls
+          */
+
+          final int ptiBatchSize = 100
+          int ptiBatchCount = 0
+          List<List<String>> ptis = batchFetchPtis(ptiBatchSize, ptiBatchCount, p.id)
+          while (ptis && ptis.size() > 0) {
+            ptiBatchCount ++
+            ptis.each { pti ->
+              generateTemplatedUrlsForPti(pti, stringTemplates, platformLocalCode)
+            }
+
+            // Next page
+            ptis = batchFetchPtis(ptiBatchSize, ptiBatchCount, p.id)
+          }
+          break;
+        case 'pti':
+          //In this case we don't have the platform, but we passed the platformId in the params
+          Platform p = Platform.get(params.platformId)
+          Map stringTemplates = findStringTemplatesForId(p.id)
+          String platformLocalCode = p.localCode
+          generateTemplatedUrlsForPti(params.id, stringTemplates, platformLocalCode)
+          break;
+        default:
+          log.warn "Don't know what to do with params context (${params.context})"
+          break;
       }
+      
     }
     log.debug "LOGDEBUG TASK END TIME"
-
-    
 
     synchronized ( this ) {
       // Task finished, turn 'running' boolean off
       running = false
 
-      if (runAgain) {
-        // While this task was running we got another trigger, so run again
-        runAgain = false
-        generateTemplatedUrlsForErmResources(final String tenantId)
+      if (taskQueue.size() > 0) {
+        // There are still tasks in the queue, run again
+        Map<String, String> runParams = taskQueue.remove(0)
+        generateTemplatedUrlsForErmResources(tenantId, runParams)
       }
     }
   }
