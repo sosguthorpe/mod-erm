5663562683f415f204ec28bb48070123d7fe48c2
Ian Ibbotson <ianibbo@googlemail.com>
2021-05-01 08:41:08 +0100

    Ii feature sample kbs (#410)
    * Added env var to run_external_reg that will enable debug output - also an example for the devops guys for how to use an env var to control debug logging
    
    * Standard sample data includes GOKB but disabled

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -1,1 +1,28 @@
 log.info "Running default tenant data file"
+
+import org.olf.kb.RemoteKB
+
+// For the generic setup - we configure GOKB_TEST but set ACTIVE=FALSE
+
+RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
+    name:'GOKb_TEST',
+    type:'org.olf.kb.adapters.GOKbOAIAdapter',
+    uri:'http://gokbt.gbv.de/gokb/oai/index',
+    fullPrefix:'gokb',
+    rectype: RemoteKB.RECTYPE_PACKAGE,
+    active:Boolean.FALSE,
+    supportsHarvesting:true,
+    activationEnabled:false
+).save(failOnError:true))
+
+RemoteKB.findByName('GOKb') ?: (new RemoteKB(
+    name:'GOKb',
+    type:'org.olf.kb.adapters.GOKbOAIAdapter',
+    uri:'https://gokborg/gokb/oai/index',
+    fullPrefix:'gokb',
+    rectype: RemoteKB.RECTYPE_PACKAGE,
+    active:Boolean.FALSE,
+    supportsHarvesting:true,
+    activationEnabled:false
+).save(failOnError:true))
+
