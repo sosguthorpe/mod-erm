7e0245b20bd294277888c6f8d3e477a8b65c5aba
Ian Ibbotson <ianibbo@gmail.com>
2022-03-04 15:24:26 +0000

    Fix clone agreement (#536)
    * Default file storage should be LOB not DB
    
    * bump toolkit version
    
    * Correct toolkit version
    
    * Be explicit about cloning DocumentAttachments FileUpload member
    
    * Increase logging level for document attachment
    
    * Try alternate approach to clone file upload
    
    * Try alternate approach to clone file upload
    
    * more info
    
    * Try explicit cast for FileUpload
    
    * logging for all Files domain classes
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>

--- service/grails-app/conf/logback.groovy
+++ service/grails-app/conf/logback.groovy
@@ -32,6 +32,7 @@ root(WARN, ['STDOUT'])
 logger ('org.hibernate.orm.deprecation', ERROR)
 logger ('com.k_int.okapi.OkapiSchemaHandler', INFO)
 logger ('org.olf.general.DocumentAttachment', DEBUG);
+logger ('com.k_int.web.toolkit.files', DEBUG);
 
 // Just increase verbosity for dev/test.
 
