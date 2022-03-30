683e68f0b201492172b5a13782dcea4601f9386c
Ian Ibbotson <ianibbo@gmail.com>
2022-03-30 16:08:41 +0100

    Don't add gokbt when adding sample data (#542)

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -4,16 +4,16 @@ import org.olf.kb.RemoteKB
 
 // For the generic setup - we configure GOKB_TEST but set ACTIVE=FALSE
 
-RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
-    name:'GOKb_TEST',
-    type:'org.olf.kb.adapters.GOKbOAIAdapter',
-    uri:'https://gokbt.gbv.de/gokb/oai/index',
-    fullPrefix:'gokb',
-    rectype: RemoteKB.RECTYPE_PACKAGE,
-    active:Boolean.FALSE,
-    supportsHarvesting:true,
-    activationEnabled:false
-).save(failOnError:true))
+// RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
+//     name:'GOKb_TEST',
+//     type:'org.olf.kb.adapters.GOKbOAIAdapter',
+//     uri:'https://gokbt.gbv.de/gokb/oai/index',
+//     fullPrefix:'gokb',
+//     rectype: RemoteKB.RECTYPE_PACKAGE,
+//     active:Boolean.FALSE,
+//     supportsHarvesting:true,
+//     activationEnabled:false
+// ).save(failOnError:true))
 
 RemoteKB.findByName('GOKb') ?: (new RemoteKB(
     name:'GOKb',
