91a12b0d34ee1465016f2d71499d6cd3ddc5699b
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2023-04-18 15:05:01 +0100

    fix: ERM-2910: JSON package import fails (#655)
    Implemented Validatable directly on the extensions of ErmPackageImpl and InternalPackageImpl
    
    ERM-2910

--- service/src/main/groovy/org/olf/dataimport/erm/ErmPackageImplWithContentItems.groovy
+++ service/src/main/groovy/org/olf/dataimport/erm/ErmPackageImplWithContentItems.groovy
@@ -1,10 +1,11 @@
 package org.olf.dataimport.erm
 
 import grails.compiler.GrailsCompileStatic
+import grails.validation.Validateable
 
 // Make distinction between ErmPackageImpl which requires contentItems and that which does not.
 @GrailsCompileStatic
-class ErmPackageImplWithContentItems extends ErmPackageImpl {
+class ErmPackageImplWithContentItems extends ErmPackageImpl implements Validateable {
   static constraints = {
     contentItems minSize: 1
   }
--- service/src/main/groovy/org/olf/dataimport/internal/InternalPackageImplWithPackageContents.groovy
+++ service/src/main/groovy/org/olf/dataimport/internal/InternalPackageImplWithPackageContents.groovy
@@ -7,11 +7,8 @@ import grails.validation.Validateable
 
 // Make distinction between InternalPackageImpl which requires packageContents and that which does not.
 @GrailsCompileStatic
-class InternalPackageImplWithPackageContents extends InternalPackageImpl {
-  
+class InternalPackageImplWithPackageContents extends InternalPackageImpl implements Validateable {
   static constraints = {
     packageContents   minSize: 1
   }
 }
-
-
