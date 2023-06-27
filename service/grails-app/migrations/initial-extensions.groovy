cb08ecc5ea38fc42d0e7352cb5faf00b0cece0bf
Ian Ibbotson <ianibbo@googlemail.com>
2023-06-27 23:09:58 +0100

    Chore grails okapi rc3 (#669)
    * Bump grails-okapi to rc3
    
    * wip
    
    * Tests passing

--- service/grails-app/migrations/initial-extensions.groovy
+++ service/grails-app/migrations/initial-extensions.groovy
@@ -1,4 +1,18 @@
 databaseChangeLog = {
+
+
+
+  changeSet(author: "sosguthorpe", id: "2023-06-27-ie-00000") {
+    grailsChange {
+      change {
+        // grailsChange gives us an sql variable which inherits the current connection, and hence should
+        // get the schema
+        // sql.execute seems to get a bit confused when passed a GString. Work it out before
+        sql.execute('CREATE EXTENSION IF NOT EXISTS btree_gin;');
+      }
+    }
+  }
+
   changeSet(author: "sosguthorpe", id: "1") {
     grailsChange {
       change {
@@ -9,4 +23,5 @@ databaseChangeLog = {
       }
     }
   }
+
 }
