565ffa4fc9869100911cba4d40e1cee3b347de5f
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2023-10-02 10:41:14 +0100

    fix(Jobs): Force new session to hopefully better tidy up memory.

--- service/grails-app/services/org/olf/PackageIngestService.groovy
+++ service/grails-app/services/org/olf/PackageIngestService.groovy
@@ -1,5 +1,7 @@
 package org.olf
 
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW
+
 import java.util.concurrent.TimeUnit
 
 import org.olf.general.StringUtils
@@ -21,6 +23,10 @@ import org.olf.kb.TitleInstance
 import org.olf.kb.MatchKey
 import org.slf4j.MDC
 
+import com.k_int.web.toolkit.utils.GormUtils
+
+import grails.gorm.multitenancy.CurrentTenant
+import grails.gorm.transactions.Transactional
 import grails.util.GrailsNameUtils
 import grails.web.databinding.DataBinder
 import groovy.util.logging.Slf4j
@@ -29,6 +35,7 @@ import groovy.util.logging.Slf4j
  * This service works at the module level, it's often called without a tenant context.
  */
 @Slf4j
+@CurrentTenant
 class PackageIngestService implements DataBinder {
 
   // This boolean controls the behaviour of the loader when we encounter a title that does not have
@@ -55,6 +62,18 @@ class PackageIngestService implements DataBinder {
 
   // For now this is repeated in pushKBService
   private static final def countChanges = ['accessStart', 'accessEnd']
+	
+	@Transactional(propagation= REQUIRES_NEW)
+	private RemoteKB findOrCreateKbByName( String kbName, boolean kbCreateReadOnly) {
+		RemoteKB kb = RemoteKB.findByName(kbName) ?: new RemoteKB(
+			name:kbName,
+		  rectype: RemoteKB.RECTYPE_PACKAGE,
+		  active: Boolean.TRUE,
+		  readonly:kbCreateReadOnly,
+		  trustedSourceTI:false).save(flush:true, failOnError:true)
+		
+		kb
+	}
 
   /**
    * Load the paackage data (Given in the agreed canonical json package format) into the KB.
@@ -65,159 +84,151 @@ class PackageIngestService implements DataBinder {
    * package into the KB.
    * @return id of package upserted
    */
-  public Map upsertPackage(PackageSchema package_data, String remotekbname, boolean readOnly=false) {
-    Pkg pkg = null;
-    // Remove MDC title at top of upsert package
-    MDC.remove('title')
-    def result = [
-      startTime: System.currentTimeMillis(),
-      titleCount: 0,
-      newTitles: 0,
-      removedTitles: 0,
-      updatedTitles: 0,
-      updatedAccessStart: 0,
-      updatedAccessEnd: 0,
-    ]
-
-    Boolean trustedSourceTI = package_data.header?.trustedSourceTI
-
-    // ERM caches many remote KB sources in it's local package inventory
-    // Look up which remote kb via the name
-    RemoteKB kb = RemoteKB.findByName(remotekbname)
-    Pkg.withNewTransaction { status ->
-
-      if (!kb) {
-       kb = new RemoteKB( name:remotekbname,
-                          rectype: RemoteKB.RECTYPE_PACKAGE,
-                          active: Boolean.TRUE,
-                          readonly:readOnly,
-                          trustedSourceTI:false).save(flush:true, failOnError:true)
-      }
-
-      if (trustedSourceTI == null) {
-        // If we're not explicitly handed trusted information, default to whatever the remote KB setting is
-        trustedSourceTI = kb.trustedSourceTI
-
-        if (trustedSourceTI == null) {
-          // If it somehow remains unset, default to false, but with warning
-          log.warn("Could not find trustedSourceTI setting for KB, defaulting to false")
-          trustedSourceTI = false
-        }
-      }
-
-      result.updateTime = System.currentTimeMillis()
-
-      log.info("Package header: ${package_data.header} - update start time is ${result.updateTime}")
-      
-      // Farm out package lookup and creation to a separate method
-      pkg = lookupOrCreatePkg(package_data);
-      // Retain logging information
-      MDC.put('packageSource', pkg.source.toString())
-      MDC.put('packageReference', pkg.reference.toString())
-
-      // Update identifiers from citation
-      identifierService.updatePackageIdentifiers(pkg, package_data.identifiers)
-
-      result.packageId = pkg.id
-    }
-
-    package_data.packageContents.eachWithIndex { ContentItemSchema pc, int index ->
-      // ENSURE MDC title is set as early as possible
-      MDC.put('title', StringUtils.truncate(pc.title.toString()))
-
-      // log.debug("Try to resolve ${pc}")
-
-      try {
-        PackageContentItem.withNewTransaction { status ->
-          // Delegate out to TitleIngestService so that any shared steps can move there.
-          Map titleIngestResult = titleIngestService.upsertTitle(pc, kb, trustedSourceTI)
-
-          // titleIngestResult.titleInstanceId will be non-null IFF TitleIngestService managed to find a title with that Id.
-          if ( titleIngestResult.titleInstanceId != null ) {
-            // Pass off to new hierarchy method (?)
-            Map hierarchyResult = lookupOrCreateTitleHierarchy(
-              titleIngestResult.titleInstanceId,
-              pkg.id,
-              trustedSourceTI,
-              pc,
-              result.updateTime,
-              result.titleCount
-            )
-
-            PackageContentItem pci = PackageContentItem.get(hierarchyResult.pciId)
-            hierarchyResultMapLogic(hierarchyResult, result, pci)
-          }
-          else {
-            // Almost the same message exists in TitleIngestService if result is null
-            String message = "Skipping \"${pc.title}\". Unable to resolve title from ${pc.title} with identifiers ${pc.instanceIdentifiers}"
-            log.error(message)
-          }
-        }
-      } catch ( Exception e ) {
-        String message = "Skipping \"${pc.title}\". System error: ${e.message}"
-        log.error(message,e)
-      }
-      result.titleCount++
-      result.averageTimePerTitle=(System.currentTimeMillis()-result.startTime)/result.titleCount
-      if ( result.titleCount % 100 == 0 ) {
-        log.debug ("Processed ${result.titleCount} titles, average per title: ${result.averageTimePerTitle}")
-      }
-    }
-    def finishedTime = (System.currentTimeMillis()-result.startTime)/1000
-
-    // This removed logic is WRONG under pushKB because it's chunked -- ensure pushKB does not call full upsertPackage method
-    // At the end - Any PCIs that are currently live (Don't have a removedTimestamp) but whos lastSeenTimestamp is < result.updateTime
-    // were not found on this run, and have been removed. We *may* introduce some extra checks here - like 3 times or a time delay, but for now,
-    // this is how we detect deletions in the package file.
-    log.debug("Remove any content items that have disappeared since the last upload. ${pkg.name}/${pkg.source}/${pkg.reference}/${result.updateTime}")
-    int removal_counter = 0
-
-    PackageContentItem.withNewTransaction { status ->
-      // FIXME we're querying on pkg itself here not pkg.id
-      PackageContentItem.executeQuery('select pci from PackageContentItem as pci where pci.pkg = :pkg and pci.lastSeenTimestamp < :updateTime and pci.removedTimestamp is null',
-                                      [pkg:pkg, updateTime:result.updateTime]).each { removal_candidate ->
-        try {
-          log.debug("Removal candidate: pci.id #${removal_candidate.id} (Last seen ${removal_candidate.lastSeenTimestamp}, thisUpdate ${result.updateTime}) -- Set removed")
-          removal_candidate.removedTimestamp = result.updateTime
-          removal_candidate.save(flush:true, failOnError:true)
-        } catch ( Exception e ) {
-          log.error("Problem removing ${removal_candidate} in package load",e)
-        }
-        result.removedTitles++
-      }
-    }
-
-    MDC.remove('recordNumber')
-    MDC.remove('title')
-    // Need to pause long enough so that the timestamps are different
-    TimeUnit.MILLISECONDS.sleep(1)
-    if (result.titleCount > 0) {
-      log.info ("Processed ${result.titleCount} titles in ${finishedTime} seconds (${finishedTime/result.titleCount} average)")
-      TimeUnit.MILLISECONDS.sleep(1)
-      log.info ("Added ${result.newTitles} titles")
-      TimeUnit.MILLISECONDS.sleep(1)
-      log.info ("Updated ${result.updatedTitles} titles")
-      TimeUnit.MILLISECONDS.sleep(1)
-      log.info ("Removed ${result.removedTitles} titles")
-      log.info ("Updated accessStart on ${result.updatedAccessStart} title(s)")
-      log.info ("Updated accessEnd on ${result.updatedAccessEnd} title(s)")
-
-      // Log the counts too.
-      for (final String change : countChanges) {
-        if (result[change]) {
-          TimeUnit.MILLISECONDS.sleep(1)
-          log.info ("Changed ${GrailsNameUtils.getNaturalName(change).toLowerCase()} on ${result[change]} titles")
-        }
-      }
-    } else {
-      if (result.titleCount > 0) {
-        log.info ("No titles to process")
-      }
-    }
-
-//    MDC.clear()
-
-    return result
+  public Map upsertPackage(PackageSchema package_data, String remotekbname, boolean kbCreateReadOnly=false) {
+		// Really messy but required as withNew session does not work unless there is already a session
+  	// bound.
+		Pkg.withSession { currentSess ->
+			Pkg.withTransaction {
+				Pkg.withNewSession { newSess ->
+					Pkg.withTransaction {
+						Pkg pkg = null;
+						// Remove MDC title at top of upsert package
+						MDC.remove('title')
+						final def result = [
+							startTime: System.currentTimeMillis(),
+							titleCount: 0,
+							newTitles: 0,
+							removedTitles: 0,
+							updatedTitles: 0,
+							updatedAccessStart: 0,
+							updatedAccessEnd: 0,
+						]
+				
+				
+						// ERM caches many remote KB sources in it's local package inventory
+						// Look up which remote kb via the name
+						final RemoteKB kb = findOrCreateKbByName(remotekbname, kbCreateReadOnly)
+						
+						final boolean trustedSourceTI = package_data.header?.trustedSourceTI ?: kb.trustedSourceTI
+						Pkg.withNewTransaction { status ->
+				
+							result.updateTime = System.currentTimeMillis()
+				
+							log.info("Package header: ${package_data.header} - update start time is ${result.updateTime}")
+							
+							// Farm out package lookup and creation to a separate method
+							pkg = lookupOrCreatePkg(package_data);
+							// Retain logging information
+							MDC.put('packageSource', pkg.source.toString())
+							MDC.put('packageReference', pkg.reference.toString())
+				
+							// Update identifiers from citation
+							identifierService.updatePackageIdentifiers(pkg, package_data.identifiers)
+				
+							result.packageId = pkg.id
+						}
+				
+						package_data.packageContents.eachWithIndex { ContentItemSchema pc, int index ->
+							// ENSURE MDC title is set as early as possible
+							MDC.put('title', StringUtils.truncate(pc.title.toString()))
+				
+							// log.debug("Try to resolve ${pc}")
+				
+							try {
+								PackageContentItem.withNewTransaction { status ->
+									// Delegate out to TitleIngestService so that any shared steps can move there.
+									Map titleIngestResult = titleIngestService.upsertTitle(pc, kb, trustedSourceTI)
+				
+									// titleIngestResult.titleInstanceId will be non-null IFF TitleIngestService managed to find a title with that Id.
+									if ( titleIngestResult.titleInstanceId != null ) {
+										// Pass off to new hierarchy method (?)
+										Map hierarchyResult = lookupOrCreateTitleHierarchy(
+											titleIngestResult.titleInstanceId,
+											pkg.id,
+											trustedSourceTI,
+											pc,
+											result.updateTime,
+											result.titleCount
+										)
+				
+										PackageContentItem pci = PackageContentItem.get(hierarchyResult.pciId)
+										hierarchyResultMapLogic(hierarchyResult, result, pci)
+									}
+									else {
+										// Almost the same message exists in TitleIngestService if result is null
+										String message = "Skipping \"${pc.title}\". Unable to resolve title from ${pc.title} with identifiers ${pc.instanceIdentifiers}"
+										log.error(message)
+									}
+								}
+							} catch ( Exception e ) {
+								String message = "Skipping \"${pc.title}\". System error: ${e.message}"
+								log.error(message,e)
+							}
+							result.titleCount++
+							result.averageTimePerTitle=(System.currentTimeMillis()-result.startTime)/result.titleCount
+							if ( result.titleCount % 100 == 0 ) {
+								log.debug ("Processed ${result.titleCount} titles, average per title: ${result.averageTimePerTitle}")
+							}
+						}
+						def finishedTime = (System.currentTimeMillis()-result.startTime)/1000
+				
+						// This removed logic is WRONG under pushKB because it's chunked -- ensure pushKB does not call full upsertPackage method
+						// At the end - Any PCIs that are currently live (Don't have a removedTimestamp) but whos lastSeenTimestamp is < result.updateTime
+						// were not found on this run, and have been removed. We *may* introduce some extra checks here - like 3 times or a time delay, but for now,
+						// this is how we detect deletions in the package file.
+						log.debug("Remove any content items that have disappeared since the last upload. ${pkg.name}/${pkg.source}/${pkg.reference}/${result.updateTime}")
+						int removal_counter = 0
+				
+						PackageContentItem.withNewTransaction { status ->
+							// FIXME we're querying on pkg itself here not pkg.id
+							PackageContentItem.executeQuery('select pci from PackageContentItem as pci where pci.pkg = :pkg and pci.lastSeenTimestamp < :updateTime and pci.removedTimestamp is null',
+																							[pkg:pkg, updateTime:result.updateTime]).each { removal_candidate ->
+								try {
+									log.debug("Removal candidate: pci.id #${removal_candidate.id} (Last seen ${removal_candidate.lastSeenTimestamp}, thisUpdate ${result.updateTime}) -- Set removed")
+									removal_candidate.removedTimestamp = result.updateTime
+									removal_candidate.save(flush:true, failOnError:true)
+								} catch ( Exception e ) {
+									log.error("Problem removing ${removal_candidate} in package load",e)
+								}
+								result.removedTitles++
+							}
+						}
+				
+						MDC.remove('recordNumber')
+						MDC.remove('title')
+						// Need to pause long enough so that the timestamps are different
+						TimeUnit.MILLISECONDS.sleep(1)
+						if (result.titleCount > 0) {
+							log.info ("Processed ${result.titleCount} titles in ${finishedTime} seconds (${finishedTime/result.titleCount} average)")
+							TimeUnit.MILLISECONDS.sleep(1)
+							log.info ("Added ${result.newTitles} titles")
+							TimeUnit.MILLISECONDS.sleep(1)
+							log.info ("Updated ${result.updatedTitles} titles")
+							TimeUnit.MILLISECONDS.sleep(1)
+							log.info ("Removed ${result.removedTitles} titles")
+							log.info ("Updated accessStart on ${result.updatedAccessStart} title(s)")
+							log.info ("Updated accessEnd on ${result.updatedAccessEnd} title(s)")
+				
+							// Log the counts too.
+							for (final String change : countChanges) {
+								if (result[change]) {
+									TimeUnit.MILLISECONDS.sleep(1)
+									log.info ("Changed ${GrailsNameUtils.getNaturalName(change).toLowerCase()} on ${result[change]} titles")
+								}
+							}
+						} else {
+							if (result.titleCount > 0) {
+								log.info ("No titles to process")
+							}
+						}
+				
+				//    MDC.clear()
+				
+						return result
+					}
+				}
+			}
+		}
   }
 
   /* 
