39c5a0389ca0b9833d3a05fec902094a0ff44573
sosguthorpe <steve.osguthorpe@k-int.com>
2023-10-04 17:39:39 +0100

    fix: Index fields (#689)

--- service/grails-app/migrations/update-mod-agreements-6-0.groovy
+++ service/grails-app/migrations/update-mod-agreements-6-0.groovy
@@ -33,4 +33,62 @@ databaseChangeLog = {
       referencedTableName: "erm_title_list"
     )
   }
+	
+	changeSet(author: "sosguthorpe (manual)", id: "20231004-001") {
+		// Gin indexes need to be done via scripting.
+		grailsChange {
+			change {
+				def cmd = "CREATE INDEX arn_name_idx ON ${database.defaultSchemaName}.alternate_resource_name USING gin (arn_name);".toString()
+				sql.execute(cmd);
+			}
+		}
+		grailsChange {
+			change {
+				def cmd = "CREATE INDEX refdata_category_rdc_description_idx ON ${database.defaultSchemaName}.refdata_category USING gin (rdc_description);".toString()
+				sql.execute(cmd);
+			}
+		}
+		grailsChange {
+			change {
+				def cmd = "CREATE INDEX identifier_id_value_idx ON ${database.defaultSchemaName}.identifier USING gin (id_value);".toString()
+				sql.execute(cmd);
+			}
+		}
+		grailsChange {
+			change {
+				def cmd = "CREATE INDEX erm_resource_res_description_idx ON ${database.defaultSchemaName}.erm_resource USING gin (res_description);".toString()
+				sql.execute(cmd);
+			}
+		}
+		
+		createIndex(indexName: "arn_owner_idx", tableName: "alternate_resource_name") {
+			column(name: "arn_owner_fk")
+		}
+		createIndex(indexName: "identifier_occurrence_io_res_fk_io_identifier_fk_idx", tableName: "identifier_occurrence") {
+			column(name: "io_res_fk")
+			column(name: "io_identifier_fk")
+		}
+		createIndex(indexName: "pkg_availability_scope", tableName: "package") {
+			column(name: "pkg_availability_scope_fk")
+		}
+		
+		createIndex(indexName: "pkg_lifecycle_status_idx", tableName: "package") {
+			column(name: "pkg_lifecycle_status_fk")
+		}
+		createIndex(indexName: "res_normalized_name_idx", tableName: "erm_resource") {
+			column(name: "res_normalized_name")
+		}
+		createIndex(indexName: "res_publication_type_idx", tableName: "erm_resource") {
+			column(name: "res_publication_type_fk")
+		}
+		createIndex(indexName: "res_sub_type_idx", tableName: "erm_resource") {
+			column(name: "res_sub_type_fk")
+		}
+		createIndex(indexName: "res_type_idx", tableName: "erm_resource") {
+			column(name: "res_type_fk")
+		}
+		createIndex(indexName: "title_instance_ti_work_fk_idx", tableName: "title_instance") {
+			column(name: "ti_work_fk")
+		}
+	}
 }
