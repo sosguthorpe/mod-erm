d3c8f92f51d0e57ace1999216a2919500a774c4f
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2024-02-21 16:58:16 +0100

    feat: URL length (#726)
    Increase length of URL for PTI to 2083 characters, to bring in line with accepted browser limits. Raised from 255 characters which caused problems in very select circumstances
    
    ERM-3155

--- service/grails-app/migrations/update-mod-agreements-6-1.groovy
+++ service/grails-app/migrations/update-mod-agreements-6-1.groovy
@@ -6,4 +6,13 @@ databaseChangeLog = {
 	changeSet(author: "efreestone (manual)", id: "20240219-1445-002") {
     dropTable(tableName: "naive_match_key_assignment_job")
   }
+
+  changeSet(author: "efreestone (manual)", id: "20240221-1120-001") {
+    modifyDataType(
+      tableName: "platform_title_instance",
+      columnName: "pti_url",
+      newDataType: "VARCHAR(2083)",
+      confirm: "Successfully updated the pti_url column."
+    )
+  }
 }
