d798eba466b9731ca6a27b35f48858e11a3e6c48
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2024-02-23 17:10:17 +0100

    test: Tweak testing environment (#729)
    Tweak the testing environment and use okapi_modules_test. This should enable this docker compose env to be used then for licenses etc

--- tools/testing/init.sql
+++ tools/testing/init.sql
@@ -2,13 +2,13 @@ select 1;
 
 CREATE USER folio_admin WITH PASSWORD 'folio_admin';
 CREATE USER folio WITH PASSWORD 'folio';
-DROP DATABASE if exists okapi_modules;
+DROP DATABASE if exists okapi_modules_test;
 DROP DATABASE if exists olfdev;
 DROP DATABASE if exists olftest;
 CREATE DATABASE olftest;
-CREATE DATABASE okapi_modules;
+CREATE DATABASE okapi_modules_test;
 CREATE DATABASE olfdev;
-GRANT ALL PRIVILEGES ON DATABASE okapi_modules to folio_admin;
+GRANT ALL PRIVILEGES ON DATABASE okapi_modules_test to folio_admin;
 GRANT ALL PRIVILEGES ON DATABASE olfdev to folio;
 GRANT ALL PRIVILEGES ON DATABASE olftest to folio;
 
