506d17ea8e6a1073eed892cd3cedb30c17a5aa09
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2024-04-11 09:56:43 +0100

    chore: GOKb_TEST (#746)
    Swap to harvesting from GOKb_TEST for the time being

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -4,18 +4,18 @@ import org.olf.kb.RemoteKB
 
 // For the generic setup - we configure GOKB_TEST but set ACTIVE=FALSE
 
-// RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
-//     name:'GOKb_TEST',
-//     type:'org.olf.kb.adapters.GOKbOAIAdapter',
-//     uri:'https://gokbt.gbv.de/gokb/oai/index',
-//     fullPrefix:'gokb',
-//     rectype: RemoteKB.RECTYPE_PACKAGE,
-//     active:Boolean.FALSE,
-//     supportsHarvesting:true,
-//     activationEnabled:false
-// ).save(failOnError:true))
+RemoteKB.findByName('GOKb_TEST') ?: (new RemoteKB(
+    name:'GOKb_TEST',
+    type:'org.olf.kb.adapters.GOKbOAIAdapter',
+    uri:'https://gokbt.gbv.de/gokb/oai/index',
+    fullPrefix:'gokb',
+    rectype: RemoteKB.RECTYPE_PACKAGE,
+    active:Boolean.TRUE,
+    supportsHarvesting:true,
+    activationEnabled:false
+).save(failOnError:true))
 
-RemoteKB.findByName('GOKb') ?: (new RemoteKB(
+/* RemoteKB.findByName('GOKb') ?: (new RemoteKB(
     name:'GOKb',
     type:'org.olf.kb.adapters.GOKbOAIAdapter',
     uri:'https://gokb.org/gokb/oai/index',
@@ -24,5 +24,5 @@ RemoteKB.findByName('GOKb') ?: (new RemoteKB(
     active:Boolean.FALSE,
     supportsHarvesting:true,
     activationEnabled:false
-).save(failOnError:true))
+).save(failOnError:true)) */
 
